3498cc09e7e48575e6262d2a568f1dde2da8456c
rifakd <rifa.khadhraoui@esprit.tn>
2025-09-30 21:23:40 +0100

    update eureka et covoiturage

--- Covoiturage/src/test/java/com/esprit/microservice/covoiturage/CovoiturageApplicationTests.java
+++ Covoiturage/src/test/java/com/esprit/microservice/covoiturage/CovoiturageApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CovoiturageApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
