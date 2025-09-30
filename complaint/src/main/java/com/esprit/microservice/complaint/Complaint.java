5f1d6dc14e17269d7e9c692f6d853dfa2ecea475
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-09-30 18:48:45 +0100

    update complaint

---
+++ complaint/src/main/java/com/esprit/microservice/complaint/Complaint.java
@@ -0,0 +1,48 @@
+package com.esprit.microservice.complaint;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    // Creation timestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+
+    @Enumerated(EnumType.STRING)
+    private ComplaintCategory category;
+
+    @Enumerated(EnumType.STRING)
+    private ComplaintStatus status;
+
+
+    // Contact information
+    private String contactEmail;
+    private String contactPhone;
+
+
+    // User who created the complaint (for permission control)
+    private String userId;
+
+
+
+}
