14637441fca3e81f92a31e28e2460c4abe76fd90
ranya.mosrtati <ranya.mosrati@esprit.tn>
2025-10-03 23:00:17 +0200

    Add Eureka to FastPost-Delivery

--- fastpost-delivery/src/main/java/com/speedygo/fastpostdelivery/FastpostDeliveryApplication.java
+++ fastpost-delivery/src/main/java/com/speedygo/fastpostdelivery/FastpostDeliveryApplication.java
@@ -2,7 +2,9 @@ package com.speedygo.fastpostdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FastpostDeliveryApplication {
 
