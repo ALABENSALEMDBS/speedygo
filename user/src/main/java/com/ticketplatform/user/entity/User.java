c76546ded83382f7e9dbcc594621c7a0d446af4a
youssef boufalgha <bufalghabusiness@gmail.com>
2025-10-19 20:56:28 +0100

    1st commit user

---
+++ user/src/main/java/com/ticketplatform/user/entity/User.java
@@ -0,0 +1,46 @@
+package com.ticketplatform.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true, unique = true)
+    private String email;
+
+    @Column(nullable = true)
+    private String firstName;
+
+    @Column(nullable = true)
+    private String lastName;
+
+    @Column(nullable = true)
+    private String password;
+
+    @Column(nullable = true)
+    private String phone;
+
+    @Column(nullable = true)
+    private String address;
+
+    @Column(nullable = true)
+    private String city;
+
+    @Column(nullable = true)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
+
